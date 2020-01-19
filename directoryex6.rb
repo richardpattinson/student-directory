201a93d1944de6b9ca2d766873d5794eb4861294
Richard <richie.pattinson@googlemail.com>
2020-01-19 13:43:58 +0000

    Excercise 6 ompleted, string centre method applied to create beautiful output.

---
+++ directoryex6.rb
@@ -0,0 +1,40 @@
+#let's put all students into an array
+students = []
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy".center(50)
+  puts "-------------".center(50)
+end
+def print(students)
+  students.each_with_index do |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort)".center(50)
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students".center(50)
+end
+#nothing happens until we call the methods
+students = input_students
+print_header
+print(students)
+print_footer(students)
