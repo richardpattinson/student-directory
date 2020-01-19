11c4dfad9369fef90285864cfa78153ab3210e09
Richard <richie.pattinson@googlemail.com>
2020-01-19 13:24:55 +0000

    Excercise 5 completed code now works with new information on candidates (hobbies, country of birth and height)

---
+++ directoryex5.rb
@@ -0,0 +1,42 @@
+#let's put all students into an array
+students = []
+
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november, hobbies: :chess,
+       Country_of_birth: :England, height:  :"5'11"
+    }
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "-------------"
+end
+def print(students)
+  students.each_with_index do |student, index|
+    puts "#{index}. #{student[:name]} (#{student[:cohort]} cohort), hobbies: (#{student[:hobbies]}), Country of Birth: (#{student[:Country_of_birth]}) height: (#{student[:height]})"
+  end
+end
+
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
+end
+#nothing happens until we call the methods
+students = input_students
+print_header
+print(students)
+print_footer(students)
